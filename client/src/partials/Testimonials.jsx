7601768fb9aeec4f19edc300f0b886ba0b58bf3f
red <dkinyanjui144@gmail.com>
2023-03-03 14:08:38 +0300

    Updated testimonials

--- client/src/partials/Testimonials.jsx
+++ client/src/partials/Testimonials.jsx
@@ -13,7 +13,7 @@ function Testimonials() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 mb-4">Don't take our word for it</h2>
-            <p className="text-xl text-gray-400">Vitae aliquet nec ullamcorper sit amet risus nullam eget felis semper quis lectus nulla at volutpat diam ut venenatis tellus—in ornare.</p>
+            <p className="text-xl text-gray-400">I have used Market De black for more tha A year now And the experience is amazing</p>
           </div>
 
           {/* Testimonials */}
